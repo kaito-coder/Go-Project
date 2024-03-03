d5a17730f7aa088aba6fdc58c59a5a1ca40afe8f
kaito <dung.hoang@codeenginestudio.com>
2024-03-03 21:05:53 +0700

    test: add unit tests for CRUD account

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 postgres:
 	docker run --name postgres12 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:12-alpine
 
+pgAdmin:
+	docker run --name pgadmin-container -p 5050:80 -e PGADMIN_DEFAULT_EMAIL=kaitok57a01@gmail.com -e PGADMIN_DEFAULT_PASSWORD=secret -d dpage/pgadmin4
+
 createdb:
 	docker exec -it postgres12 createdb --username=root --owner=root simple_bank
 
@@ -14,4 +17,8 @@ migratedown:
 	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose down
 sqlc:
 	sqlc generate
+
+test:
+	go test -v -cover ./...
+
 .PHONY: postgres createdb dropdb migrateup migratedown sqlc
---
+++ db/sqlc/account_test.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"github.com/stretchr/testify/require"
+	"simple-bank/until"
+	"testing"
+	"time"
+)
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+
+}
+func createRandomAccount(t *testing.T) Account {
+	account, err := testQueries.CreateAccount(context.Background(), CreateAccountParams{
+		Owner:    until.RandomOwner(),
+		Balance:  until.RandomMoney(),
+		Currency: until.RandomCurrency(),
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	return account
+}
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: until.RandomMoney(),
+	}
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.Empty(t, account2)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+}
+func TestListAccount(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
